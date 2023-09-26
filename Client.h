3bafd3c4bb1f3cd83d31c37c836d7256c1f70d30
Rbeautelus1 <103546272+Rbeautelus1@users.noreply.github.com>
2023-09-26 14:28:31 -0400

    Update Client.h

--- Client.h
+++ Client.h
@@ -1,12 +1,28 @@
-/*
-In this file you must create your class and prototype your public functions.
-*/
-
-class Client
+struct  Client
 { 
     private:
         //define your private members here
-
+        string client_name;
+        string ssn;
+        double bank_account;
+        double balance;
+    
     public:
         //define your public members here
+    void set_client_info(string );
+    void get_client_info();
+    double deposit(double account_number, double amount);
+    string withdraw(double account_number, double amount);
+    void saving_info(string file_name);
+    void find_client(double account_number)
+    Client(){}; //constructor
+    Client(string file_name);
+    ~Client();
+};
+
+Class Bank::public Client
+{
+    private:
+    int num_client;
+    Client **client_info;
 };
