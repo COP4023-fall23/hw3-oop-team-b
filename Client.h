71a71bc818a77136ddbb792528b0331605be433c
Rbeautelus1 <103546272+Rbeautelus1@users.noreply.github.com>
2023-09-29 11:55:49 -0400

    Update Client.h

--- Client.h
+++ Client.h
@@ -1,3 +1,10 @@
+#idndef CLIENT_H
+#define CLIENT_H
+
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
 struct  Client
 { 
     private:
@@ -6,23 +13,26 @@ struct  Client
         string ssn;
         double bank_account;
         double balance;
-    
-    public:
-        //define your public members here
-    void set_client_info(string );
-    void get_client_info();
-    double deposit(double account_number, double amount);
-    string withdraw(double account_number, double amount);
-    void saving_info(string file_name);
-    void find_client(double account_number)
-    Client(){}; //constructor
-    Client(string file_name);
-    ~Client();
 };
 
-Class Bank::public Client
+Class Bank
 {
     private:
     int num_client;
-    Client **client_info;
+    Client *client_info;
+    int index;
+
+    public:
+        //define your public members here
+    void set_client_info(const string& client_name, const string& ssn, double bank_account, double balance);
+    Client* get_client_info();
+    double deposit(int account_number, double amount);
+    string withdraw(int account_number, double amount);
+    void saving_info(const string& file_name);
+    void find_client(int account_number)
+    Bank(const string& filename); //constructor
+Bank(const Bank& other);
+    ~Bank();
 };
+
+#endif
